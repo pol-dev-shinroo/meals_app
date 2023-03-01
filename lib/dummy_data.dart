6b2ed65ce6cd2de57f2791c93b420f25578e478e
pol-dev-shinroo <manus.dream.language@gmail.com>
2023-03-01 14:37:46 +0900

    category_item + models

---
+++ lib/dummy_data.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import "./models/category.dart";
+
+const dummyData = [
+  Category(id: "c1", title: "Italian", color: Colors.purple),
+  Category(id: "c2", title: "Asian", color: Colors.purple),
+  Category(id: "c3", title: "African", color: Colors.purple),
+  Category(id: "c4", title: "German", color: Colors.purple),
+];
